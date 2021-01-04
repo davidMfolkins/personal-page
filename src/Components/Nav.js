bd1623c045535c21f24a6a8a21e0a8a931f4d058
David Folkins <david.folkins88@gmail.com>
2021-01-04 14:12:14 -0500

    Add css to links

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,17 @@
+import './App.scss';
+import { Link } from 'react-router-dom';
+
 function Nav() {
   return (
     <nav>
       <h3>Logo</h3>
       <ul className="nav-links">
-        <li>About</li>
-        <li>Projects</li>
+        <Link className="text-link" to='/about'>
+          <li>About</li>
+        </Link>
+        <Link className="text-link" to='/projects'>
+          <li>Projects</li>
+        </Link>
       </ul>
     </nav>
   );
