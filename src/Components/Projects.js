b381529a2709eb54a38bd8fe86bf14e372d46850
David Folkins <david.folkins88@gmail.com>
2021-01-05 14:28:19 -0500

    Add placeholder content to projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,31 @@
+import './Projects.scss';
+
 function Projects() {
   return (
     <div className="projects-container">
       <h1 className="projects-title">Projects</h1>
+      <div className="project-grid">
+        <div>
+          <img src="https://via.placeholder.com/300x200"></img>
+          <p>Project1</p>
+          <p>description</p>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/300x200"></img>
+          <p>Project2</p>
+          <p>description</p>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/300x200"></img>
+          <p>Project3</p>
+          <p>description</p>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/300x200"></img>
+          <p>Project4</p>
+          <p>description</p>
+        </div>
+      </div>
     </div>
   );
 }
