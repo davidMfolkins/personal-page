372d2236b5ee99d5e84041a710932b5b02369a7c
David Folkins <david.folkins88@gmail.com>
2021-01-06 13:14:30 -0500

    Add description to PoE armoury

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,8 +7,8 @@ function Projects() {
       <div className="project-grid">
         <div className="project-container">
           <img src="https://via.placeholder.com/400x250"></img>
-          <p>Project1</p>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p>PoE Armoury</p>
+          <p>A character database for the video game Path of Exile. Users can search characters, browse their inventory/skills, and save to favourites!</p>
         </div>
         <div className="project-container">
           <img src="https://via.placeholder.com/400x250"></img>
