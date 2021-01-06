a1c54adbe102afb2e75c170c663569d42bfdc69b
David Folkins <david.folkins88@gmail.com>
2021-01-06 13:17:47 -0500

    Add description to Tweeter

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -22,8 +22,8 @@ function Projects() {
         </div>
         <div className="project-container">
           <img src="https://via.placeholder.com/400x250"></img>
-          <p>Project4</p>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p>Tweeter</p>
+          <p>Tweeter is a simple, single-page Twitter clone. This was part of the Lighthouse Labs Curriculum</p>
         </div>
       </div>
     </div>
