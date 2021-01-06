5f124c749591c6710243afcafb51abe90b4089cc
David Folkins <david.folkins88@gmail.com>
2021-01-06 13:16:51 -0500

    Add description to Schedular

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -17,8 +17,8 @@ function Projects() {
         </div>
         <div className="project-container">
           <img src="https://via.placeholder.com/400x250"></img>
-          <p>Project3</p>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p>Schedular</p>
+          <p>Interview Scheduler is a single-page full stack React scheduler. Users can book appointments, edit those appointments and cancel them! This was part of Lighthouse Labs Curriculum</p>
         </div>
         <div className="project-container">
           <img src="https://via.placeholder.com/400x250"></img>
