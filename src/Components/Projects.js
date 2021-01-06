a7b684dced7e903114c9a271e099718ed9996b93
David Folkins <david.folkins88@gmail.com>
2021-01-06 14:00:04 -0500

    add real desciption to each project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,22 +8,22 @@ function Projects() {
         <div className="project-container">
           <img src="https://raw.githubusercontent.com/davidMfolkins/PoE-Armoury/master/docs/filters_large.png" width="600" height="350"></img>
           <p>PoE Armoury</p>
-          <p>A character database for the video game Path of Exile. Users can search characters, browse their inventory/skills, and save to favourites!</p>
+          <p>A character database for the video game Path of Exile. Users can search account names, see the account's characters and browse their inventory/skills. Users can also create an account and favoruite certain character builds to view at another time.</p>
         </div>
         <div className="project-container">
           <img src="https://via.placeholder.com/600x350"></img>
           <p>mApp</p>
-          <p>A node app with jQuery/AJAX, express, and PostgresQL where users can create public maps and build a personal profile.</p>
+          <p>A node app with jQuery/AJAX, express, and PostgresQL where users can create public maps. User can drop pins on their map, describe what is there and add pictures. Users can publish these maps, favourite them and also contribute to eachother maps</p>
         </div>
         <div className="project-container">
           <img src="https://raw.githubusercontent.com/davidMfolkins/scheduler/master/public/docs/book-interview.png" width="600" height="350"></img>
           <p>Schedular</p>
-          <p>Interview Scheduler is a single-page full stack React scheduler. Users can book appointments, edit those appointments and cancel them! This was part of Lighthouse Labs Curriculum</p>
+          <p>Interview Scheduler is a single-page full stack React scheduler. Users can book appointments, edit those appointments and cancel them! This was part of Lighthouse Labs curriculum</p>
         </div>
         <div className="project-container">
           <img src="https://raw.githubusercontent.com/davidMfolkins/tweeter/master/public/docs/desktop-mode.png" width="600" height="350"></img>
           <p>Tweeter</p>
-          <p>Tweeter is a simple, single-page Twitter clone. This was part of the Lighthouse Labs Curriculum</p>
+          <p>Tweeter is a simple, single-page Twitter clone. User can tweet and see their tweets display automatically. This was part of the Lighthouse Labs curriculum</p>
         </div>
       </div>
     </div>
