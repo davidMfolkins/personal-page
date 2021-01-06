972d471e5ec8b15a806f6442d398f894437a09bb
David Folkins <david.folkins88@gmail.com>
2021-01-06 13:15:50 -0500

    Add description to mApp

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,8 +12,8 @@ function Projects() {
         </div>
         <div className="project-container">
           <img src="https://via.placeholder.com/400x250"></img>
-          <p>Project2</p>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p>mApp</p>
+          <p>A node app with jQuery/AJAX, express, and PostgresQL where users can create public maps and build a personal profile.</p>
         </div>
         <div className="project-container">
           <img src="https://via.placeholder.com/400x250"></img>
